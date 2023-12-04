980918d4fef0d6edb990980c00e7cee8599bc956
YorkshireDev <YorkshireDev@protonmail.com>
2023-12-04 18:24:53 +0000

    Day 3 - Part 2

---
+++ src/Day_03/Main_Part_2.java
@@ -0,0 +1,106 @@
+package Day_03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static common.Read.read;
+
+public class Main_Part_2 {
+
+    private static int[] getAdjPair(int inputListIndex, int gearIndex, List<String> inputList) {
+
+        int[] adjPair = new int[] {-1, -1};
+
+        int adjCount = 0;
+
+        List<String> searchList = new ArrayList<>();
+
+        if (inputListIndex > 0) searchList.add(inputList.get(inputListIndex - 1));
+        searchList.add(inputList.get(inputListIndex));
+        if (inputListIndex < inputList.size() - 1) searchList.add(inputList.get(inputListIndex + 1));
+
+        for (String searchStr : searchList) {
+
+            searchStr += ".";
+
+            StringBuilder numBuilder = new StringBuilder();
+            List<Integer> digitIndexList = new ArrayList<>();
+            boolean isNum = false;
+
+            for (int i = 0; i < searchStr.length(); i++) {
+
+                char currentChar = searchStr.charAt(i);
+
+                if (Character.isDigit(currentChar)) {
+
+                    numBuilder.append(currentChar);
+                    digitIndexList.add(i);
+                    isNum = true;
+
+                } else {
+
+                    if (isNum) {
+
+                        for (int dIndex : digitIndexList) {
+
+                            if (dIndex == gearIndex - 1 || dIndex == gearIndex || dIndex == gearIndex + 1) {
+                                adjPair[adjCount++] = Integer.parseInt(numBuilder.toString());
+                                if (adjCount == 2) return adjPair;
+                                break;
+                            }
+
+                        }
+
+                        numBuilder = new StringBuilder();
+                        digitIndexList.clear();
+                        isNum = false;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return adjPair;
+
+    }
+
+    private static int calculate(List<String> inputList) {
+
+        int result = 0;
+
+        for (int inputListIndex = 0; inputListIndex < inputList.size(); inputListIndex++) {
+
+            String inputString = inputList.get(inputListIndex);
+
+            for (int charIndex = 0; charIndex < inputString.length(); charIndex++) {
+
+                char currentChar = inputString.charAt(charIndex);
+
+                if (currentChar == '*') {
+
+                    int[] adjPair = getAdjPair(inputListIndex, charIndex, inputList);
+
+                    if (adjPair[1] == -1) continue;
+
+                    result += (adjPair[0] * adjPair[1]);
+
+                }
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(calculate(read("Day_03", "input.txt")));
+
+    }
+
+}
