e894b35d945fff85ed96e616a70000634a0f95b1
YorkshireDev <YorkshireDev@protonmail.com>
2023-12-06 17:51:54 +0000

    Create Template

---
+++ src/common/Template.java
@@ -0,0 +1,21 @@
+package common;
+
+import java.util.List;
+
+public class Template {
+
+    private static int calculate(List<String> inputList) {
+
+        int result = 0;
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(calculate(Read.read("Day_XX", "input.txt")));
+
+    }
+
+}
