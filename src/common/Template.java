d2882672e271d37ad944738f87cb2245e4b250d4
YorkshireDev <YorkshireDev@protonmail.com>
2023-12-07 00:07:55 +0000

    Update Template

--- src/common/Template.java
+++ src/common/Template.java
@@ -1,33 +1,36 @@
-package common;
+/**
+ * Template for me to copy and paste.
+ * Commented out because IntelliJ complains that it's unused.
+ */
 
-import java.util.List;
-
-import static common.Read.read;
-
-public class Template {
-
-    private static int calculate(List<String> inputList) {
-
-        int result = 0;
-
-        return result;
-
-    }
-
-    public static void main(String[] args) {
-
-        double sTime, eTime;
-
-        sTime = System.nanoTime();
-
-        System.out.println(calculate(read("Day_XX", "input.txt")));
-
-        eTime = System.nanoTime() - sTime;
-        eTime /= 1_000_000.0d;
-
-        System.out.println();
-        System.out.println("Time Taken: " + eTime + "ms");
-
-    }
-
-}
+//import java.util.List;
+//
+//import static common.Read.read;
+//
+//public class Template {
+//
+//    private static int calculate(List<String> inputList) {
+//
+//        int result = 0;
+//
+//        return result;
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        double sTime, eTime;
+//
+//        sTime = System.nanoTime();
+//
+//        System.out.println(calculate(read("Day_XX", "input.txt")));
+//
+//        eTime = System.nanoTime() - sTime;
+//        eTime /= 1_000_000.0d;
+//
+//        System.out.println();
+//        System.out.println("Time Taken: " + eTime + "ms");
+//
+//    }
+//
+//}
