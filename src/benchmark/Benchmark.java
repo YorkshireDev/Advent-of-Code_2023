a062112cb9f4dc4e8df74e980cf4746d51d27cf0
YorkshireDev <YorkshireDev@protonmail.com>
2023-12-08 19:15:52 +0000

    Add Automatic Benchmark

---
+++ src/benchmark/Benchmark.java
@@ -0,0 +1,69 @@
+package benchmark;
+
+/**
+ * Run every test one after the other, printing the result. May as well manually add them since it's only 2 per day.
+ * <p>
+ * Each test benchmarks itself and will print the time taken in milliseconds.
+ */
+public class Benchmark {
+
+    public static void main(String[] args) {
+
+        System.out.println("---- Day_01 - Part 1 + 2 ----");
+        System.out.println();
+        Day_01.Main.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_02 - Part 1 ----");
+        System.out.println();
+        Day_02.Main_Part_1.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_02 - Part 2 ----");
+        System.out.println();
+        Day_02.Main_Part_2.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_03 - Part 1 ----");
+        System.out.println();
+        Day_03.Main_Part_1.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_03 - Part 2 ----");
+        System.out.println();
+        Day_03.Main_Part_2.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_04 - Part 1 ----");
+        System.out.println();
+        Day_04.Main_Part_1.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_04 - Part 2 ----");
+        System.out.println();
+        Day_04.Main_Part_2.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_05 - Part 1 ----");
+        System.out.println();
+        Day_05.Main_Part_1.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_05 - Part 2 ----");
+        System.out.println();
+        Day_05.Main_Part_2.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_06 - Part 1 ----");
+        System.out.println();
+        Day_06.Main_Part_1.main(new String[0]);
+        System.out.println();
+
+        System.out.println("---- Day_06 - Part 2 ----");
+        System.out.println();
+        Day_06.Main_Part_2.main(new String[0]);
+        System.out.println();
+
+    }
+
+}
