a674fa1c6f6b7406140cef592272a618d7b6352d
YorkshireDev <YorkshireDev@protonmail.com>
2023-12-18 23:15:39 +0000

    Day 7 - Part 2

--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -11,62 +11,67 @@ public class Benchmark {
 
         System.out.println("---- Day_01 - Part 1 + 2 ----");
         System.out.println();
-        Day_01.Main.main(new String[0]);
+        Day_01.Main.main(args);
         System.out.println();
 
         System.out.println("---- Day_02 - Part 1 ----");
         System.out.println();
-        Day_02.Main_Part_1.main(new String[0]);
+        Day_02.Main_Part_1.main(args);
         System.out.println();
 
         System.out.println("---- Day_02 - Part 2 ----");
         System.out.println();
-        Day_02.Main_Part_2.main(new String[0]);
+        Day_02.Main_Part_2.main(args);
         System.out.println();
 
         System.out.println("---- Day_03 - Part 1 ----");
         System.out.println();
-        Day_03.Main_Part_1.main(new String[0]);
+        Day_03.Main_Part_1.main(args);
         System.out.println();
 
         System.out.println("---- Day_03 - Part 2 ----");
         System.out.println();
-        Day_03.Main_Part_2.main(new String[0]);
+        Day_03.Main_Part_2.main(args);
         System.out.println();
 
         System.out.println("---- Day_04 - Part 1 ----");
         System.out.println();
-        Day_04.Main_Part_1.main(new String[0]);
+        Day_04.Main_Part_1.main(args);
         System.out.println();
 
         System.out.println("---- Day_04 - Part 2 ----");
         System.out.println();
-        Day_04.Main_Part_2.main(new String[0]);
+        Day_04.Main_Part_2.main(args);
         System.out.println();
 
         System.out.println("---- Day_05 - Part 1 ----");
         System.out.println();
-        Day_05.Main_Part_1.main(new String[0]);
+        Day_05.Main_Part_1.main(args);
         System.out.println();
 
         System.out.println("---- Day_05 - Part 2 ----");
         System.out.println();
-        Day_05.Main_Part_2.main(new String[0]);
+        Day_05.Main_Part_2.main(args);
         System.out.println();
 
         System.out.println("---- Day_06 - Part 1 ----");
         System.out.println();
-        Day_06.Main_Part_1.main(new String[0]);
+        Day_06.Main_Part_1.main(args);
         System.out.println();
 
         System.out.println("---- Day_06 - Part 2 ----");
         System.out.println();
-        Day_06.Main_Part_2.main(new String[0]);
+        Day_06.Main_Part_2.main(args);
         System.out.println();
 
         System.out.println("---- Day_07 - Part 1 ----");
         System.out.println();
-        Day_07.Main_Part_1.main(new String[0]);
+        Day_07.Main_Part_1.main(args);
+        System.out.println();
+
+        System.out.println("---- Day_07 - Part 2 ----");
+        System.out.println();
+        Day_07.Main_Part_2.main(args);
         System.out.println();
 
     }
